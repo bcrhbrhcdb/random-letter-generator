a6cfcedc58f4f0ed75899e6c954bcff734826220
bcrhbrhcdb <apet2804@mpsedu.org>
2024-09-19 10:32:26 -0500

    it works lets fucking go

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 //import
-
 import { symbols } from "./symbols.js";
 
 //get stuff from index.html
-
 const generateButton = document.getElementById("generateSymbol");
 const generatedSymbol = document.getElementById("generatedSymbol");
-const multipleSymbol = document.getElementById("generateMultipleSymbols")
+const multipleSymbolButton = document.getElementById("generateMultipleSymbols");
 
 //generate random symbol
-
 generateButton.addEventListener('click', ()=>{
     const randomIndex = Math.floor(Math.random() * symbols.length);
     generatedSymbol.innerText = symbols[randomIndex];
+    console.log(generatedSymbol);
 })
 
 //multiple symbols
+multipleSymbolButton.addEventListener("click", () => {
+    const multipleSymbolSelect = document.getElementById("moreSymbols");
+    const numberOfSymbols = parseInt(multipleSymbolSelect.value);
+    let generatedSymbols = '';
 
-multipleSymbol.addEventListener("click",()=>{
-    const multipleSymbol = document.getElementById("multipleSymbol");
-    const numberOfSymbols = 0;
-    switch (numberOfSymbols){
-        
+    if (isNaN(numberOfSymbols)) {
+        generatedSymbol.innerText = "Please select a number of symbols";
+        return;
     }
-    const randomMultipleIndex = Math.floor(Math.random() * symbols.length);
- })
- 
+
+    for (let i = 0; i < numberOfSymbols; i++) {
+        const randomIndex = Math.floor(Math.random() * symbols.length);
+        generatedSymbols += symbols[randomIndex];
+    }
+
+    generatedSymbol.innerText = generatedSymbols;
+})
